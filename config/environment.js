0cf57dac75de2034b9fe021fd06d29a3dbca15dd
Manisha Patel <manisha_patel@apple.com>
2016-04-10 00:42:24 -0700

    up to lesson 4

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'library-app',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://burning-inferno-4129.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
